39f9fa7816b14a2ab86b02fc07d24cc5e5f719a0
Kadir ÖZDEMİR <Kadirozdemirr00@gmail.com>
2023-09-27 01:22:35 +0300

    update menu

--- alan-hesaplama-uygulamasi-solution/alan-hesaplama-uygulamasi/Program.cs
+++ alan-hesaplama-uygulamasi-solution/alan-hesaplama-uygulamasi/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+int secim;
+
+do
+{
+
+    Console.Clear();
+
+    Console.WriteLine("Alan Hesaplama Uygulaması");
+    Console.WriteLine("-------------------------");
+    Console.WriteLine();
+
+    Console.WriteLine("Menü");
+    Console.WriteLine("----");
+    Console.WriteLine("1) Kare Alanı Hesapla");
+    Console.WriteLine("2) Dikdörtgen Alanı Hesapla");
+    Console.WriteLine("3) Üçgen Alanı Hesapla");
+    Console.WriteLine("4) Yamuk Alanı Hesapla");
+    Console.WriteLine("0) Çıkış");
+    Console.WriteLine();
+
+    Console.Write("Seçiminiz : ");
+    secim = int.Parse(Console.ReadLine());
+
+
+} while (secim != 0);
