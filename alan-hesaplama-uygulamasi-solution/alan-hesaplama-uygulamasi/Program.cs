614c65ab62bd15d74bb94a9db8a6fe7be872c4ba
Kadir ÖZDEMİR <Kadirozdemirr00@gmail.com>
2023-09-27 01:34:57 +0300

    update kare-alan-hesaplama

--- alan-hesaplama-uygulamasi-solution/alan-hesaplama-uygulamasi/Program.cs
+++ alan-hesaplama-uygulamasi-solution/alan-hesaplama-uygulamasi/Program.cs
@@ -7,12 +7,8 @@ do
 
     Console.Clear();
 
-    Console.WriteLine("Alan Hesaplama Uygulaması");
-    Console.WriteLine("-------------------------");
-    Console.WriteLine();
-
-    Console.WriteLine("Menü");
-    Console.WriteLine("----");
+    EkranaBaslikYaz("Alan Hesaplama Uyguluması");
+    EkranaBaslikYaz("Menü");
     Console.WriteLine("1) Kare Alanı Hesapla");
     Console.WriteLine("2) Dikdörtgen Alanı Hesapla");
     Console.WriteLine("3) Üçgen Alanı Hesapla");
@@ -23,5 +19,61 @@ do
     Console.Write("Seçiminiz : ");
     secim = int.Parse(Console.ReadLine());
 
+    switch (secim)
+    {
+        case 1:
+            // Kare Alan Hesaplama
+            KareAlanHesaplama();
+            break;
+
+        case 2:
+            // Dikdörtgen Alan Hesaplama
+
+            break;
+
+        case 3:
+            // Üçgen Alan Hesaplama
+
+            break;
+
+        case 4:
+            // Yamuk Alan Hesaplama
+
+            break;
+
+        default:
+            break;
+    }
+
 
 } while (secim != 0);
+
+
+static void EkranaBaslikYaz(string baslik)
+{
+    Console.WriteLine(baslik);
+
+    for (int i = 0; i < baslik.Length; i++)
+    {
+        Console.Write("-");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+static void KareAlanHesaplama()
+{
+    Console.Clear();
+
+    EkranaBaslikYaz("Kare Alan Hesaplama");
+    Console.Write("Lütfen a kenar uzunluğu giriniz : ");
+    int a = int.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+    Console.WriteLine("Kare alanı : " + (a * a));
+
+
+    Console.WriteLine("Lütfen devam etmek için bir tuşa basınız.");
+    Console.ReadKey();
+}
